f2ae9a8a3c9cc3d3380618b26d5d0c2a0111516d
Oscar Chang <oscarchang1226@gmail.com>
2017-08-23 17:09:20 -0500

    add match prepareQuestion

--- js/matching.type.js
+++ js/matching.type.js
@@ -8,26 +8,47 @@ ochang @ Smith & Associates
 let Matching = (function() {
     "use strict";
 
+    const INST = `
+        Match each item to the appropriate category. For keyboard only users,
+        use the tab key to select an answer from the list and use the enter key to select it.
+        Use tab again to select the correct dropzone and then hit the enter key to confirm your answer.
+        Select an answer and hit the delete key to return it to its original position.
+        Use the "Check Answers" button to get your results.
+    `;
+
     return {
         prepareQuestion,
         prepareAnswer,
         prepareFeedback
     };
 
-    function prepareQuestion(general, data, containerRef) {
-        
+    function prepareQuestion(general, data) {
+        return [
+            prepareInstructions(general.showInstruction),
+            createElement('p', data.questionText, 'question')
+        ];
     }
 
-    function prepareAnswer(general, data, containerRef) {
+    function prepareAnswer(general, data) {
 
     }
 
-    function prepareFeedback(general, data, containerRef) {
+    function prepareFeedback(general, data) {
 
     }
 
-    function prepareInstruction(general, containerRef) {
+    function prepareInstructions(show=true) {
+        if (show) {
+            return createElement('p', INST, 'instructions');
+        }
+        return null;
+    }
 
+    function createElement(tag, text, className="") {
+        let el = document.createElement(tag);
+        el.innerHTML = text;
+        el.className = className;
+        return el;
     }
 
 })();
--- js/quiz.js
+++ js/quiz.js
@@ -1,7 +1,13 @@
 (function(){
     "use strict";
 
-    console.log(Matching);
-    console.log(matchTypeDummy);
+    let $ = jQuery;
+
+    console.log($('.header'));
+    let questionEl = Matching.prepareQuestion({}, matchTypeDummy);
+    questionEl.forEach(q => {
+        $('.header').append(q);
+    });
+    // console.log(matchTypeDummy);
 
 })();
