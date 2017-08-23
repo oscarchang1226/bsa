a49b7c4dba75114e15a741f74d7853982b9f2a73
Oscar Chang <oscarchang1226@gmail.com>
2017-08-23 16:44:31 -0500

    start matching type

---
+++ data/dummy.js
@@ -0,0 +1,61 @@
+let matchTypeDummy = {
+    "questionType":"Matching",
+    "maxScoreValue": 4,
+    "questionText":"Match the Pricing Type to the brief description to test your understanding.",
+    "hintText":"",
+    "hintMedia":"none",
+    "AnswerNodes": [
+        {
+            "key": "ppv",
+            "title": "Purchase Price Variance (PPV)",
+            "color": "default",
+            "src" : "none"
+        },
+        {
+            "key": "price",
+            "title": "Direct, Standard, or Contract Cost/Price",
+            "color": "default",
+            "src" : "none"
+        },
+        {
+            "key": "markup",
+            "title": "Mark-up",
+            "color": "default",
+            "src": "none"
+        }
+    ],
+    "QuestionNodes": [
+        {
+            "question" : "The exact cost or price paid under contract for components.",
+            "answer" : "price",
+            "type" : "text",
+            "color": "default",
+            "correct" : "",
+            "wrong" : ""
+        },
+        {
+            "question" : "The difference between the base cost/price and the current or market cost.",
+            "answer" : "ppv",
+            "type" : "text",
+            "color": "default",
+            "correct" : "",
+            "wrong" : ""
+        },
+        {
+            "question" : "The amount of profit added to the standard cost/price.",
+            "answer" : "markup",
+            "type" : "text",
+            "color": "default",
+            "correct" : "",
+            "wrong" : ""
+        },
+        {
+            "question" : "The price that the market will bear for a component at a particular time.",
+            "answer" : null,
+            "type" : "text",
+            "color": "default",
+            "correct" : "",
+            "wrong" : "This is known as Market Price"
+        }
+    ]
+}
---
+++ js/matching.type.js
@@ -0,0 +1,33 @@
+/**
+
+Matching Quiz Type
+August 2017
+
+ochang @ Smith & Associates
+**/
+let Matching = (function() {
+    "use strict";
+
+    return {
+        prepareQuestion,
+        prepareAnswer,
+        prepareFeedback
+    };
+
+    function prepareQuestion(general, data, containerRef) {
+        
+    }
+
+    function prepareAnswer(general, data, containerRef) {
+
+    }
+
+    function prepareFeedback(general, data, containerRef) {
+
+    }
+
+    function prepareInstruction(general, containerRef) {
+
+    }
+
+})();
