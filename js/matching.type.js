1d22c047f4a3968d237d4b1e570a00f3f4da4643
Oscar Chang <oscarchang1226@gmail.com>
2017-08-25 11:51:17 -0500

    add drag n drop

--- js/matching.type.js
+++ js/matching.type.js
@@ -15,10 +15,16 @@ var Matching = (function() {
         Use the "Check Answers" button to get your results.
     `;
 
+    var validationProvider = {};
+
     return {
         prepareQuestion: prepareQuestion,
         prepareAnswer: prepareAnswer,
-        prepareFeedback: prepareFeedback
+        prepareFeedback: prepareFeedback,
+        onDragStart: onDragStart,
+        onDrop: onDrop,
+        onDragOver: onDragOver,
+        checkAnswer: checkAnswer
     };
 
     function prepareQuestion(general, data) {
@@ -29,7 +35,11 @@ var Matching = (function() {
     }
 
     function prepareAnswer(general, data) {
-
+        return [
+            prepareQuestionNodes(data.QuestionNodes),
+            prepareAnswerNodes(data.AnswerNodes),
+            prepareAnswerActions()
+        ]
     }
 
     function prepareFeedback(general, data) {
@@ -47,8 +57,10 @@ var Matching = (function() {
 
     function createElement(tag, text, options=null) {
         var el = document.createElement(tag);
-        var text = document.createTextNode(text);
-        el.appendChild(text);
+        if (text) {
+            var text = document.createTextNode(text);
+            el.appendChild(text);
+        }
         if (options && typeof(options) === 'object') {
             Object.keys(options).forEach(o => {
                 el[o] = options[o];
@@ -57,5 +69,90 @@ var Matching = (function() {
         return el;
     }
 
+    function prepareAnswerNodes(data) {
+        var container = createElement('div', null, {className: 'answer-nodes-container'});
+        // {
+        //     "key": "ppv",
+        //     "title": "Purchase Price Variance (PPV)",
+        //     "color": "default",
+        //     "src" : "none"
+        // }
+        data.forEach(answerNode => {
+            var node = createElement('div', null, {className: 'answer-nodes'});
+            var title = createElement('h4', answerNode.title);
+            var ul = createElement('ul', null, {
+                className: 'dropzone',
+                id: answerNode.key
+            });
+            node.appendChild(title);
+            node.appendChild(ul);
+            node.setAttribute('ondragover', 'Matching.onDragOver(event)');
+            node.setAttribute('ondrop', 'Matching.onDrop(event)');
+            container.appendChild(node);
+        });
+        return container;
+    }
+
+    function prepareQuestionNodes(data) {
+        var container = createElement('ul', null, {className: 'question-nodes-container dropzone'});
+        // {
+        //     "question" : "The exact cost or price paid under contract for components.",
+        //     "answer" : "price",
+        //     "type" : "text",
+        //     "color": "default",
+        //     "correct" : "",
+        //     "wrong" : ""
+        // }
+        data.forEach((questionNode, idx) => {
+            var li = createElement('li', questionNode.question, {
+                className: 'questionNode',
+                id: idx
+            });
+            li.setAttribute('draggable', 'true');
+            li.setAttribute('ondragstart', 'Matching.onDragStart(event)');
+            container.appendChild(li);
+            if(questionNode.answer) {
+                if(validationProvider[questionNode.answer]) {
+                    validationProvider[questionNode.answer].push(idx);
+                } else {
+                    validationProvider[questionNode.answer] = [idx];
+                }
+            }
+        });
+        container.setAttribute('ondragover', 'Matching.onDragOver(event)');
+        container.setAttribute('ondrop', 'Matching.onDrop(event)');
+        return container;
+    }
+
+    function prepareAnswerActions() {
+        var el = createElement('div', null, {className: 'answer-actions'});
+        var button = createElement('button', 'Check Answer');
+        button.setAttribute('onclick', 'Matching.checkAnswer()');
+        el.appendChild(button);
+        return el;
+    }
+
+    function checkAnswer() {
+        console.log(validationProvider);
+    }
+
+    function onDragStart(ev) {
+        ev.dataTransfer.setData('nodeId', ev.currentTarget.id);
+    }
+
+    function onDrop(ev) {
+        ev.preventDefault();
+        var nodeId = ev.dataTransfer.getData('nodeId');
+        var node = document.getElementById(nodeId);
+        if (ev.currentTarget.querySelector('ul.dropzone')) {
+            ev.currentTarget.querySelector('ul.dropzone').appendChild(node);
+        } else {
+            ev.currentTarget.appendChild(node);
+        }
+    }
+
+    function onDragOver(ev) {
+        ev.preventDefault();
+    }
 
 })();
--- js/quiz.js
+++ js/quiz.js
@@ -4,8 +4,12 @@
 
     console.log($('.header'));
     var questionEl = Matching.prepareQuestion({}, matchTypeDummy);
+    var answerEl = Matching.prepareAnswer({}, matchTypeDummy);
     questionEl.forEach(q => {
         $('.header').append(q);
+    });
+    answerEl.forEach(a => {
+        $('.content').append(a);
     });
     // console.log(matchTypeDummy);
 
