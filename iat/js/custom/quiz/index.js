2070bc2412a166da288a8f98e5f3e5ade98598f5
Oscar Chang <oscarchang1226@gmail.com>
2017-11-10 19:17:10 -0600

    add iat

---
+++ iat/js/custom/quiz/index.js
@@ -0,0 +1,240 @@
+/**
+
+IAT plugins
+
+**/
+/*jslint browser:true*/
+/*global console, InlineQuizApp, CSVal, SMI, Smith, d2log, jQuery */
+(function ($, q, c, v, d) {
+    'use strict';
+
+    if (c && !c.user) {
+        try {
+            c.init();
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    if (q) {
+        /**
+         * Transform response data to quiz data
+        **/
+        q.attemptData = { questions: {} };
+        q.transformApiQuizData = function (data) {
+            var result = {
+                "General": {
+                    "QuizName": (data.assessment.name || "Assessment") + '<span></span>',
+                    "feedBackType": "continuous",
+                    "forceCorrect": false,
+                    "repeatOnComplete": true,
+                    "allowNone": false,
+                    "allowPrevious": false,
+                    "showHints": false,
+                    "allowPartial": true,
+                    "randomize": true,
+                    "subtractWrong": true,
+                    "postQuizText": 'You have completed this assessment.',
+                    "timer": data.assessment.timer_in_minutes
+                },
+                'Questions': data.questions.map(function (q) {
+                    // Correct any flaws
+                    var correctAnswers = q.answers.filter(function (a) {
+                        return a.scoreValue;
+                    });
+
+                    // Correct quiz type names
+                    if (q.questionType.toLowerCase() === 'fill in the blank') {
+                        q.questionType = 'Fill In The Blank';
+                    } else if (q.questionType.toLowerCase() === 'multiple select') {
+                        q.questionType = 'All That Apply';
+                    }
+
+                    // Correct quiz answers
+                    q.answers = q.answers.map(function (a) {
+                        if (a.scoreValue) {
+                            a.scoreValue = Number((q.maxScoreValue / correctAnswers.length).toFixed(2));
+                        }
+                        return a;
+                    });
+
+                    return q;
+                })
+            };
+            if (data.assessment.name.toLowerCase().indexOf('quiz') > -1) {
+                result.General.preQuizText = "<p>Take a few minutes to self-test what you have just learned before the end of this course. At the end of each course, you'll take a Final Test that covers all the material in the course. You need a 100% to pass, so practicing now will help you succeed. This self-study quiz is NOT graded and feedback is given immediately to help you understand mistakes.</p>";
+            } else if (data.assessment.name.toLowerCase().indexOf('test') > -1) {
+                result.General.preQuizText = "<p>This is your Final Test for this course. Please make sure that you have reviewed the material in the videos and in the written portions of the course. Please also make sure that you have done the review quiz to practice the types of questions you may encounter in this Final Test.<br />";
+                result.General.preQuizText += "<br />You will need to get a 100% score on this Test to pass and earn credit (and a badge) for completing the course. If you pass the Test in your first attempt, you will earn five (5) points and a badge. Each attempt after the first, will deducted one point from the possible five for passing. These points add up in the <strongSmith U Leaderboard</strong>, which is a company-wide leaderboard based on individuals and on offices.<br/>";
+                result.General.preQuizText += "<br />After you have passed all of the Tests for all of the graded courses in a series, you will take the Final Exam for the entire series. The Final Exam for this series is TR109: Final Exam.<br />";
+                result.General.preQuizText += "<br />Good luck!</p>";
+                delete result.General.repeatOnComplete;
+            }
+
+            result.General.preQuizText += '<p>Press Start Activity to begin the assessment.</p>';
+            return result;
+        };
+
+        q.setupQuizWithApi = function (elId) {
+            var temp = location.search.replace('?', '').split('&'),
+                context = {},
+                vm = this;
+            temp.forEach(function (t) {
+                context[t.split('=')[0]] = t.split('=')[1];
+            });
+            if (CSVal && CSVal.user.Identifier) {
+                context.ui = CSVal.user.Identifier;
+                context.taker_first = CSVal.user.FirstName;
+                context.taker_last = CSVal.user.LastName;
+            } else {
+                context.ui = 221;
+                context.taker_first = 'Oscar';
+                context.taker_last = 'Chang';
+            }
+            v.init(context);
+            d.getAssessment(context.assessmentId, function (res) {
+                v.currentContext.assessmentId = res.responseJSON.assessment.id;
+                if (document.getElementById(elId) !== null) {
+                    vm.containerRef = document.getElementById(elId);
+                    vm.QuizData = vm.transformApiQuizData(res.responseJSON);
+                    vm.BuildQuiz(vm.QuizData);
+                } else {
+                    d2log('ERROR: Missing specified DOM object in InlineQuizApp.setupQuizWithApi().');
+                }
+            });
+        };
+
+        q.onComplete = function (data) {
+            /**
+                data    scoreAchieved
+                        scoreMax
+                        quizData
+            **/
+            // d.updateAttempt(v.currentContext.attempt_id, q.attemptData, function () {
+            //     console.log('update grade, issue award', v.currentContext.gi, v.currentContext.ai);
+            // });
+            q.QuizData.General.postQuizText += '<p>You scored <strong>' + (data.scoreAchieved / data.scoreMax * 100).toFixed(2) + '%</strong> (' + data.scoreAchieved + ' out of ' + data.scoreMax + ').</p>';
+        };
+
+        q.onPostComplete = function () {
+            q.updateTimer();
+            q.stopTimer();
+        };
+
+        q.onNextTopic = function () {
+            if (parent && parent.document) {
+                var nextButton = parent.document.querySelector('a.d2l-iterator-button-next');
+                if (nextButton) {
+                    parent.document.querySelector('a.d2l-iterator-button-next').click();
+                } else {
+                    console.warn('Cannot find next button');
+                }
+            }
+        };
+
+        q.onReset = function () {
+            $(q.containerRef).fadeOut(500);
+            $(q.containerRef).empty();
+            $(q.containerRef).fadeIn(500);
+            q.setupQuizWithApi(q.containerRef.id);
+        };
+
+        q.onCheckAnswer = function (data) {
+            q.attemptData.questions[data.question.questionId] = {
+                score: data.qScore,
+                time: 30
+            };
+            console.log('archive question/answers');
+        };
+
+        q.onReady = function (data) {
+            // console.log(data.quizData.General.timer);
+            if (data.quizData.General.timer) {
+                q.QuizData.General.timer = data.quizData.General.timer * 60;
+                q.timer = {
+                    m: Math.floor(q.QuizData.General.timer / 60),
+                    s: q.QuizData.General.timer % 60
+                };
+                q.updateTimer();
+            }
+        };
+
+        q.onStart = function () {
+            // d.storeAttempt({
+            //     taker_id: v.currentContext.ui,
+            //     taker_first: v.currentContext.taker_first,
+            //     taker_last: v.currentContext.taker_last,
+            //     module_id: v.currentContext.ou,
+            //     assessment_id: v.currentContext.assessmentId
+            // }, function (res) {
+            //     v.currentContext.attempt_id = res.responseJSON.id;
+            //     q.GoNextQuestion();
+            // });
+            q.startTimer();
+            q.GoNextQuestion();
+        };
+
+        q.getTimeInSeconds = function () {
+            // var currentTime = q.timer.m * 60 + q.timer.s;
+            Object.keys(q.attemptData.questions).reduce(function (acc, qId) {
+                console.log(q.attemptData.questions[qId]);
+                return acc + q.attemptData.questions[qId].time;
+            }, 0);
+            return q.QuizData.General.timer - Object.keys(q.attemptData.questions).reduce(function (acc, qId) { return acc + q.attemptData.questions[qId].time; }, 0);
+        };
+
+        q.updateTimer = function (s) {
+            if (q.QuizData.General.timer) {
+                $('#ILQ_header h1').css({'position': 'relative'});
+                $('#ILQ_header h1 span').css({'position': 'absolute', 'right': '5pt'});
+                $('#ILQ_header h1 span').html(s || q.formatTimer(q.timer));
+            }
+        };
+
+        q.formatTimer = function (obj) {
+            var a = obj.m > 9 ? String(obj.m) : '0' + obj.m,
+                b = obj.s > 9 ? String(obj.s) : '0' + obj.s;
+            return a + ':' + b;
+        };
+
+        q.decrementTimer = function () {
+            if (q.QuizData.General.timer && q.timer.s > -1) {
+                q.timer.s -= 1;
+                if (q.timer.s < 0) {
+                    q.timer.m -= 1;
+                    if (q.timer.m < 0) {
+                        // Times up
+                        console.log('Times up');
+                    } else {
+                        q.timer.s = 59;
+                    }
+                }
+            }
+        };
+
+        q.startTimer = function () {
+            if (q.QuizData.General.timer && !q.timer.interval) {
+                q.timer.interval = setInterval(function () {
+                    if (q.timer.s === 0 && q.timer.m === 0) {
+                        // endQuiz();
+                        // currentIndex = quizData.Questions.length-1;
+                        q.currentQuestion = q.QuizData.General.showQuestions;
+                        q.updateTimer('00:00');
+                        q.goEndSlide();
+                    } else {
+                        q.decrementTimer();
+                        q.updateTimer();
+                    }
+                },
+                    1000
+                    );
+            }
+        };
+
+        q.stopTimer = function () {
+            if (q.timer.interval) {
+                clearInterval(q.timer.interval);
+            }
+        };
+    }
+}(jQuery, InlineQuizApp, CSVal, SMI, Smith));
---
+++ iat/js/custom/valence/index.js
@@ -0,0 +1,145 @@
+/*
+ * Smith and Associates package, js api.
+ *
+ * Copyright (c) 2017 N.F. Smith & Associates
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the license at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+/*
+ * This file provides services for Valence Learning Framework API.
+ */
+
+/*jslint browser: true */
+/*global D2L, jQuery, $, console, SMI */
+(function ($) {
+    "use strict";
+    window.SMI = {
+        LEVERSION: '1.25',
+        BAVERSION: '1.0',
+        LPVERSION: '1.18',
+        currentContext: {},
+        init: function (currentContext) {
+            this.currentContext = currentContext;
+        },
+        getUrls: function (name) {
+            var urls = {
+                user_grade: '/d2l/api/le/' + this.LEVERSION + '/' + this.currentContext.ou + '/grades/' + this.currentContext.gi + '/values/' + this.currentContext.ui,
+                grade: '/d2l/api/le/' + this.LEVERSION + '/' + this.currentContext.ou + '/grades/' + this.currentContext.gi,
+                grade_stats: '/d2l/api/le/' + this.LEVERSION + '/' + this.currentContext.ou + '/grades/' + this.currentContext.gi + '/statistics',
+                associations: '/d2l/api/bas/' + this.BAVERSION + '/orgunits/' + this.currentContext.ou + '/associations/',
+                issue_award: '/d2l/api/bas/' + this.BAVERSION + '/orgunits/' + this.currentContext.ou + '/issued/',
+                who_am_i: '/d2l/api/lp/' + this.LPVERSION + '/users/whoami'
+            };
+            return urls[name];
+        },
+        getAppContext: function () {
+            var a = 'Bocs5Yg-9rEpxK_0L4dGUw',
+                b = 'zEoByoY5l3sWLvBkwXAmcA';
+            return new D2L.ApplicationContext(a, b);
+        },
+        getUserContext: function () {
+            var c = 'https://smithweb.brightspace.com',
+                d = '443',
+                e = 'e9RfZgHDTu3vm_gqMPsmfC',
+                f = 'DphdEgK0jpS_8P-ZXaZQWR';
+            return this.getAppContext().createUserContextWithValues(c, d, e, f);
+        },
+        preCall: function (cb) {
+            if (this.currentContext.prod) {
+                $.get('/d2l/lp/auth/xsrf-tokens', cb);
+            } else {
+                cb({'hitCodePrefix': '-1315665569', 'referrerToken': 'Znpl0OTAb63pYv88SK92DaKkwZsArud9'});
+            }
+        },
+        callAjax: function (m, u, cb, data) {
+            if (typeof cb !== 'function') {
+                cb = function (res, err) {
+                    if (res.status !== 200) {
+                        console.error(res.statusText, err);
+                    }
+                };
+            }
+            u = this.getUserContext().createUrlForAuthentication(u, m);
+            var preCallback = function (d) {
+                var settings = {
+                    type: m,
+                    url: u,
+                    complete: cb,
+                    headers: {
+                        'X-Csrf-Token': d.referrerToken
+                    }
+                };
+                if ('POST' === m.toUpperCase() || 'PUT' === m.toUpperCase()) {
+                    settings.dataType = 'json';
+                    settings.contentType = "application/json";
+                    settings.data = JSON.stringify(data);
+                    settings.headers.Accept = 'application/json';
+                    settings.headers['Content-Type'] = 'application/json';
+                }
+                $.ajax(settings);
+            };
+            return this.preCall(preCallback);
+        },
+        getAssociations: function (cb) {
+            var url = this.getUrls('associations');
+            return this.callAjax('GET', url, cb);
+        },
+        issueAward: function (cb, data) {
+            var url = this.getUrls('issue_award');
+            return this.callAjax('POST', url, cb, data);
+        },
+        getGrade: function (cb) {
+            var url = this.getUrls('grade');
+            return this.callAjax('GET', url, cb);
+        },
+        getGradeStats: function (cb) {
+            var url = this.getUrls('grade_stats');
+            return this.callAjax('GET', url, cb);
+        },
+        getUserGrade: function (cb) {
+            var url = this.getUrls('user_grade');
+            return this.callAjax('GET', url, cb);
+        },
+        putUserGrade: function (cb, data) {
+            var url = this.getUrls('user_grade');
+            return this.callAjax('PUT', url, cb, data);
+        },
+        generateIssuedAwardCreate: function (c, e) {
+            return {
+                AwardId: this.currentContext.ai,
+                IssuedToUserId: this.currentContext.ui,
+                Criteria: c,
+                Evidence: e
+            };
+        },
+        generateIncomingGradeValue: function (score, c, pc) {
+            return {
+                GradeObjectType: 1,
+                PointsNumerator: score,
+                Comments: {
+                    Content: c || '',
+                    Type: 'Text'
+                },
+                PrivateComments: {
+                    Content: pc || '',
+                    Type: 'Text'
+                }
+            };
+        },
+        whoAmI: function (cb) {
+            var url = this.getUrls('who_am_i');
+            return this.callAjax('GET', url, cb);
+        }
+    };
+}(jQuery));
